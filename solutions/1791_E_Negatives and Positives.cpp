74c4f6a33ed48cd4eb324f3ee9964930bcade6d7
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2023-03-07 12:40:52 +0530

    Create 1791_E_Negatives and Positives.cpp

---
+++ solutions/1791_E_Negatives and Positives.cpp
@@ -0,0 +1,70 @@
+// Author - Vedant Valsangkar
+// 2023 CP GOALS - Expert at CF and Guardian at Leetcode
+// I WILL MAKE IT HAPPEN
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template<class T> using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+typedef long long    ll;
+typedef long double  ld;
+#define int          long long
+#define rep(i,s,e)   for(int i=s;i<e;i++)
+#define repr(i,s,e)  for(int i=s;i>=e;i--)
+#define in(a)        for(auto &i:a) cin>>i
+#define out(a)       for(auto &i:a) cout<<i<<" "; cout<<endl
+#define uniq(a)      a.erase(unique(all(a)),a.end())
+#define all(a)       a.begin(),a.end()
+#define allr(a)      a.rbegin(),a.rend()
+#define endl         '\n'
+
+bool per_square(ld x){if(x<0) return 0; ll sr=sqrt(x); return (sr*sr==x);}
+bool is_prime(ll x){for(int i=2;i*i<=x;i++) if(x%i==0) return 0; return 1;}
+
+void solve()
+{   
+    int n; cin>>n;
+    vector<int> a(n); in(a);
+    int f0 = 0;
+    for(auto x: a) if(!x) f0 = 1;
+    if(f0) {
+        int sum = 0;
+        rep(i,0,n) sum += abs(a[i]);
+        cout<<sum<<endl;
+        return;
+    }
+    int mn = 1e9, neg = 0;
+    for(auto x: a) {
+        if(x < 0) neg++;
+        mn = min(mn, abs(x));
+    }
+    if(neg%2==0) {
+        int sum = 0;
+        rep(i,0,n) sum += abs(a[i]);
+        cout<<sum<<endl;
+    } else {
+        int sum = 0;
+        rep(i,0,n) sum += abs(a[i]);
+        sum -= 2*mn;
+        cout<<sum<<endl;
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    // sieve();
+    int tc = 1;
+    cin>>tc;
+    for(int t=1;t<=tc;t++) {
+        // cout<<"Case #"<<t<<": ";
+        solve();
+    }
+    return 0;
+}
