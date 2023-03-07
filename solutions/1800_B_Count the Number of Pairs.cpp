910d3839030040bde979c2056653b617d710570b
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2023-03-07 12:32:30 +0530

    Create 1800_B_Count the Number of Pairs.cpp

---
+++ solutions/1800_B_Count the Number of Pairs.cpp
@@ -0,0 +1,60 @@
+// Author - Vedant Valsangkar
+// 2023 CP GOALS - Expert at CF and Guardian at Leetcode
+// I WILL MAKE IT HAPPEN
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template<class T> using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+typedef long long    ll;
+typedef long double  ld;
+#define int          long long
+#define rep(i,s,e)   for(int i=s;i<e;i++)
+#define repr(i,s,e)  for(int i=s;i>=e;i--)
+#define in(a)        for(auto &i:a) cin>>i
+#define out(a)       for(auto &i:a) cout<<i<<" "; cout<<endl
+#define uniq(a)      a.erase(unique(all(a)),a.end())
+#define all(a)       a.begin(),a.end()
+#define allr(a)      a.rbegin(),a.rend()
+#define endl         '\n'
+
+bool per_square(ld x){if(x<0) return 0; ll sr=sqrt(x); return (sr*sr==x);}
+bool is_prime(ll x){for(int i=2;i*i<=x;i++) if(x%i==0) return 0; return 1;}
+
+void solve()
+{   
+    int n,k; cin>>n>>k;
+    string s; cin>>s;
+    map<char, int> mp;
+    for(auto ch: s) mp[ch]++;
+    int ans = 0;
+    for(char ch='a'; ch<='z'; ch++) {
+        char ch2 = ch-32;
+        int y = min(mp[ch], mp[ch2]);
+        ans += y;
+        mp[ch] -= y; mp[ch2] -= y;
+        int mx = max(mp[ch], mp[ch2]);
+        if(k >= mx/2) ans+=mx/2,k-=mx/2;
+        else ans+=k,k=0;
+    }
+    cout<<ans<<endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    // sieve();
+    int tc = 1;
+    cin>>tc;
+    for(int t=1;t<=tc;t++) {
+        // cout<<"Case #"<<t<<": ";
+        solve();
+    }
+    return 0;
+}
