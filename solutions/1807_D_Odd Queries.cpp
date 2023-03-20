afd47173d4dd493b0b512eea55ca9b5cfbfa80e0
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2023-03-20 10:20:39 +0530

    Create 1807_D_Odd Queries.cpp

---
+++ solutions/1807_D_Odd Queries.cpp
@@ -0,0 +1,70 @@
+// Author - Vedant Valsangkar
+// BELIEVE
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define int long long
+#define rep(i, s, e) for (int i = s; i < e; i++)
+#define repr(i, s, e) for (int i = s; i >= e; i--)
+#define uniq(a) a.erase(unique(all(a)), a.end())
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define endl '\n'
+
+template <typename T>
+istream& operator>>(istream& istream, vector<T>& v) {
+    for (auto& it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T>
+ostream& operator<<(ostream& ostream, const vector<T>& c) {
+    for (auto& it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+bool per_square(ld x) {
+    if (x < 0) return 0;
+    ll sr = sqrt(x);
+    return (sr * sr == x);
+}
+bool is_prime(ll x) {
+    for (int i = 2; i * i <= x; i++)
+        if (x % i == 0) return 0;
+    return 1;
+}
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n), pre(n + 1, 0);
+    cin >> a;
+    rep(i, 1, n + 1) pre[i] = pre[i - 1] + a[i - 1];
+    while (q--) {
+        int l, r, k;
+        cin >> l >> r >> k;
+        int cur_sum = pre[r] - pre[l - 1];
+        int new_sum = k * (r - l + 1);
+        int total_sum = pre[n] - cur_sum + new_sum;
+        if (total_sum & 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout<<"Case #"<<t<<": ";
+        solve();
+    }
+    return 0;
+}
