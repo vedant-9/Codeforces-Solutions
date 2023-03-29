f3e35e230cbad29f7a7d8659114056aea633b3e3
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2023-03-29 22:51:43 +0530

    Create 1808_B_Playing in a Casino.cpp

---
+++ solutions/1808_B_Playing in a Casino.cpp
@@ -0,0 +1,74 @@
+// Author - Vedant Valsangkar
+// BELIEVE
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define int long long
+#define rep(i, s, e) for (int i = s; i < e; i++)
+#define repr(i, s, e) for (int i = s; i >= e; i--)
+#define uniq(a) a.erase(unique(all(a)), a.end())
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define endl '\n'
+
+template <typename T>
+istream& operator>>(istream& istream, vector<T>& v) {
+    for (auto& it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T>
+ostream& operator<<(ostream& ostream, const vector<T>& c) {
+    for (auto& it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+bool per_square(ld x) {
+    if (x < 0) return 0;
+    ll sr = sqrt(x);
+    return (sr * sr == x);
+}
+bool is_prime(ll x) {
+    for (int i = 2; i * i <= x; i++)
+        if (x % i == 0) return 0;
+    return 1;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(m, vector<int>(n, 0));
+    rep(i, 0, n) {
+        rep(j, 0, m) {
+            cin >> a[j][i];
+        }
+    }
+    rep(i, 0, m) {
+        sort(all(a[i]));
+    }
+    int ans = 0;
+    rep(i, 0, m) {
+        int cur = 0;
+        rep(j, 0, n) {
+            ans += a[i][j] * j - cur;
+            cur += a[i][j];
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout<<"Case #"<<t<<": ";
+        solve();
+    }
+    return 0;
+}
