7d3e78fb6eb4205597630e252d28bb0bc180fe3d
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2023-06-12 23:06:34 +0530

    Create 1841_B_Keep it Beautiful.cpp

---
+++ solutions/1841_B_Keep it Beautiful.cpp
@@ -0,0 +1,82 @@
+// Author - Vedant Valsangkar
+// BELIEVE
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define int long long
+#define rep(i, s, e) for (int i = s; i < e; i++)
+#define repr(i, s, e) for (int i = s; i >= e; i--)
+#define uniq(a) a.erase(unique(all(a)), a.end())
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define endl '\n'
+
+template <typename T>
+istream& operator>>(istream& istream, vector<T>& v) {
+    for (auto& it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T>
+ostream& operator<<(ostream& ostream, const vector<T>& c) {
+    for (auto& it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+bool per_square(ld x) {
+    if (x < 0) return 0;
+    ll sr = sqrt(x);
+    return (sr * sr == x);
+}
+bool is_prime(ll x) {
+    for (int i = 2; i * i <= x; i++)
+        if (x % i == 0) return 0;
+    return 1;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    string ans(n, '1');
+    cin >> a;
+    int flag = 0;
+    if (a[0] > a[1]) flag = 1;
+    int prev = a[1];
+    rep(i, 2, n) {
+        if (!flag and prev <= a[i]) {
+            prev = a[i];
+            continue;
+        }
+        if (!flag) {
+            if (a[i] <= a[0]) {
+                prev = a[i];
+                flag = 1;
+            } else
+                ans[i] = '0';
+        } else {
+            if (a[i] <= a[0] and prev <= a[i]) {
+                prev = a[i];
+                continue;
+            } else
+                ans[i] = '0';
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout<<"Case #"<<t<<": ";
+        solve();
+    }
+    return 0;
+}
