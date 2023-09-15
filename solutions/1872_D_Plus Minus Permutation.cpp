c67d722c9987ed83fc9244363cc4c2b1314d43ef
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2023-09-15 11:54:26 +0530

    Create 1872_D_Plus Minus Permutation.cpp

---
+++ solutions/1872_D_Plus Minus Permutation.cpp
@@ -0,0 +1,66 @@
+// Author - Vedant Valsangkar
+// BELIEVE
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define int long long
+#define rep(i, s, e) for (int i = s; i < e; i++)
+#define repr(i, s, e) for (int i = s; i >= e; i--)
+#define uniq(a) a.erase(unique(all(a)), a.end())
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define endl '\n'
+
+template <typename T>
+istream& operator>>(istream& istream, vector<T>& v) {
+    for (auto& it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T>
+ostream& operator<<(ostream& ostream, const vector<T>& c) {
+    for (auto& it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+bool per_square(ld x) {
+    if (x < 0) return 0;
+    ll sr = sqrt(x);
+    return (sr * sr == x);
+}
+bool is_prime(ll x) {
+    for (int i = 2; i * i <= x; i++)
+        if (x % i == 0) return 0;
+    return 1;
+}
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+int lcm(int a, int b) { return a * b / (gcd(a, b)); }
+
+void solve() {
+    int n, x, y;
+    cin >> n >> x >> y;
+    int lc = lcm(x, y);
+    int addcnt = n / x - n / lc;
+    int subcnt = n / y - n / lc;
+    cout << addcnt * n - addcnt * (addcnt - 1) / 2 - subcnt * (subcnt + 1) / 2 << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout<<"Case #"<<t<<": ";
+        solve();
+    }
+    return 0;
+}
