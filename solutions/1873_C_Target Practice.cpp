1977b5ce69f5bd2d8b81557e189db26e1a6e62af
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2023-09-22 17:10:11 +0530

    Create 1873_C_Target Practice.cpp

---
+++ solutions/1873_C_Target Practice.cpp
@@ -0,0 +1,78 @@
+// Author - Vedant Valsangkar
+// BELIEVE
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define int long long
+#define rep(i, s, e) for (int i = s; i < e; i++)
+#define repr(i, s, e) for (int i = s; i >= e; i--)
+#define uniq(a) a.erase(unique(all(a)), a.end())
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define endl '\n'
+
+template <typename T>
+istream& operator>>(istream& istream, vector<T>& v) {
+    for (auto& it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T>
+ostream& operator<<(ostream& ostream, const vector<T>& c) {
+    for (auto& it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+bool per_square(ld x) {
+    if (x < 0) return 0;
+    ll sr = sqrt(x);
+    return (sr * sr == x);
+}
+bool is_prime(ll x) {
+    for (int i = 2; i * i <= x; i++)
+        if (x % i == 0) return 0;
+    return 1;
+}
+
+vector<vector<int>> b;
+
+void solve() {
+    vector<string> a(10);
+    cin >> a;
+    int ans = 0;
+    rep(i, 0, 10) {
+        rep(j, 0, 10) {
+            if (a[i][j] == 'X') {
+                ans += b[i][j];
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    b = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+        {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+        {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+        {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+        {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+        {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout<<"Case #"<<t<<": ";
+        solve();
+    }
+    return 0;
+}
